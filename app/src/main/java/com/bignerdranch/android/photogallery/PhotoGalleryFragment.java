fed2a2b690cd95cf3591fc95d649d0b78419adcd
Chris Dziewa <chris.dziewa@gmail.com>
2017-04-03 11:12:18 -0500

    Complete chapter 25 with final challenge - dynamic column sizing

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +28,7 @@ public class PhotoGalleryFragment extends Fragment {
     private RecyclerView mPhotoRecyclerView;
     private List<GalleryItem> mItems = new ArrayList<>();
     private int mPageNumber = 1;
+    private int mNumColumns = 3;
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -46,7 +48,7 @@ public class PhotoGalleryFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 
         mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.photo_recycler_view);
-        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), mNumColumns));
         mPhotoRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
@@ -55,13 +57,26 @@ public class PhotoGalleryFragment extends Fragment {
                 int totalItems = lm.getItemCount();
                 int lastVisibleItem = lm.findLastVisibleItemPosition();
 
-                if ((lastVisibleItem + 6) >= totalItems && mPageNumber < 10) {
+                if ((lastVisibleItem + 10) >= totalItems && mPageNumber < 10) {
                     // Call api and append items (Temporarily replace)
                     mPageNumber++;
                     new FetchItemsTask().execute();
                 }
             }
         });
+
+        // Layout listener for updating column count based on width at runtime
+        mPhotoRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                GridLayoutManager manager = (GridLayoutManager) mPhotoRecyclerView.getLayoutManager();
+                float currentWidth = manager.getWidth();
+                mNumColumns = (int) currentWidth / 300;
+                manager.setSpanCount(mNumColumns);
+                Toast.makeText(getActivity(), "Num of columns: " + mNumColumns, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         setupAdapter();
         return v;
     }
@@ -133,7 +148,6 @@ public class PhotoGalleryFragment extends Fragment {
                 int oldSize = mItems.size();
                 mItems.addAll(galleryItems);
                 Log.i(TAG, "onPostExecute: added " + mItems.size() + " items");
-                Toast.makeText(getActivity(), "Got more items!", Toast.LENGTH_SHORT).show();
                 mPhotoRecyclerView.getAdapter().notifyItemRangeInserted(oldSize, galleryItems.size());
             }
         }
